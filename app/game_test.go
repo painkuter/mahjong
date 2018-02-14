c3aea6c9e88f9b3dddd471db923ac4adc56c9890
kutepov <sergey.kutepo@lazada.com>
2018-02-14 17:27:58 +0300

    Find kong and chow

--- app/game_test.go
+++ app/game_test.go
@@ -11,5 +11,31 @@ func TestFindPong(t *testing.T) {
 	b := "1_1_2"
 	c := "1_1_3"
 	h := hand{"1_1_1", "1_2_3", "1_1_2", "1_1_3"}
-	assert.Equal(t, h.findPong(a,b,c), true)
+	assert.Equal(t, h.findPong([]string{a, b, c}), true)
+}
+
+func TestFindPong_NotFound(t *testing.T) {
+	a := "1_1_1"
+	b := "1_1_2"
+	c := "1_1_3"
+	h := hand{"1_1_1", "1_2_3", "1_1_3"}
+	assert.Equal(t, h.findPong([]string{a, b, c}), false)
+}
+
+func TestFindKong(t *testing.T) {
+	a := "1_1_1"
+	b := "1_1_2"
+	c := "1_1_3"
+	d := "1_1_4"
+	h := hand{"1_1_1", "1_2_3", "1_1_2", "1_1_3", "1_1_4"}
+	assert.Equal(t, h.findKong([]string{a, b, c, d}), true)
+}
+
+func TestFindKong_NotFound(t *testing.T) {
+	a := "1_1_1"
+	b := "1_1_2"
+	c := "1_1_3"
+	d := "1_1_4"
+	h := hand{"1_1_1", "1_2_3", "1_1_3", "1_1_4"}
+	assert.Equal(t, h.findKong([]string{a, b, c, d}), false)
 }
